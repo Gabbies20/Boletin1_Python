36e0e72e781467190fc620a0bd46f4b2518b6b29
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-03 02:19:21 +0200

    Finalizado hasta el ejercicio 10

---
+++ eje8.py
@@ -0,0 +1,4 @@
+#Imprime los numeros del 1 al 10 usando un bucle for.
+
+for i in range(1,10+1):
+    print(i)
---
+++ eje9.py
@@ -0,0 +1,7 @@
+#Calcula la suma de los números del 1 al 100. 
+
+suma=0
+
+for i in range (1,100+1):
+    suma += i
+print(f"La suma de los números es {suma}")
